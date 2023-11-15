3622cfe64652f5329be000da1e1eb030baaff5d0
pili swalehe <swalehe_pili@yahoo.com>
2023-11-15 15:05:38 -0800

    Task 1

--- shell.h
+++ shell.h
@@ -18,6 +18,10 @@ int interactive(info_t *info);
 int _atoi(char *s);
 int _isalpha(int c);
 int _isdelim(char c, char *delimeter);
+char *_strchr(char *s, char c);
+char *_strcat(char *dest, char *src);
+int _strcmp(char *s1, char *s2);
+int _strlen(char *s);
 
 
 /* Builtin command struct*/
