33b5deba7d72a0ba87103d49811192a273bda01f
pili swalehe <swalehe_pili@yahoo.com>
2023-11-15 15:55:13 -0800

    merge

--- path.c
+++ path.c
@@ -1,42 +1,63 @@
 #include "shell.h"
 
 /**
- * display_prompt - displays the output
- *
- * Return: output
+ * check_cmd - Excutes commands found in predefined path
+ * @cmd: Array of parsed command strings
+ * @input: Input recieved from user (to be freed)
+ * @c:Shell Excution Time Case of Command Not Found
+ * @argv: Arguments before program starts(argv[0] == Shell Program Name)
+ * Return: 1 Case Command Null -1 Wrong Command 0 Command Excuted
  */
-void display_prompt(void)
+int check_cmd(char **cmd, char *input, int c, char **argv)
 {
-	printf("simple_shell$ ");
-	fflush(stdout);
+	int status;
+	pid_t pid;
+
+	if (*cmd == NULL)
+		return (EXIT_FAILURE);
+	pid = fork();
+	if (pid == -1)
+	{
+		perror("Error");
+		return (-1);
+	}
+	if (pid == 0)
+	{
+		if (_strncmp(*cmd, "./", 2) != 0 && _strncmp(*cmd, "/", 1) != 0)
+			path_cmd(cmd);
+		if (access(cmd[0], R_OK) != 0)
+		{
+			print_error(cmd[0], c, argv);
+			free_all(cmd, input);
+			exit(127);
+		}
+		if (execve(*cmd, cmd, environ) == -1)
+			return (2);
+		else
+			return (0);
+	}
+	wait(&wstatus);
+	if (WIFEXITED(wstatus))
+	{
+		if (WEXITSTATUS(wstatus) == 0)
+			return (0);
+		else if (WEXITSTATUS(wstatus) == 2)
+			return (2);
+		else if (WEXITSTATUS(wstatus) == 127)
+			return (127);
+	}
+	return (127);
 }
 
 /**
- * main - entry point
- *
- * Return: 0
+ * signal_to_handle - Configures ^C not to terminate our shell
+ * @sig: Incoming Signal
  */
-int main(void)
+void signal_to_handle(int sig)
 {
-	char command[100];
-
-	while (1)
+	if (sig == SIGINT)
 	{
-		display_prompt();
-		if (fgets(command, sizeof(command), stdin) == NULL)
-		{
-			printf("\nexit simple_shell\n");
-			break;
-		}
-		if (strchr(command, ' ') != NULL)
-		{
-			printf("Error: Command must be a single word.\n");
-			continue;
-		}
-		if (execlp(command, command, (char *)NULL) == -1)
-		{
-			printf("Error: '%s' not found\n", command)
-		}
+		PRINT("\n$ ");
 	}
-	return (0);
 }
+
