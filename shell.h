78f942f3ee3cd8b060615db830fb03af4b1ba993
pili swalehe <swalehe_pili@yahoo.com>
2023-11-15 14:27:56 -0800

    task 1

--- shell.h
+++ shell.h
@@ -1,6 +1,8 @@
 #ifndef _SHELL_H_
 #define _SHELL_H_
 
+/* Standard libraries*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -11,50 +13,27 @@
 #include <limits.h>
 #include <fcntl.h>
 
-typedef struct liststr
-{
-	int num;
-	char *str;
-	struct liststr *next;
-} list_t;
+/*string handler functions*/
+int interactive(info_t *info);
+int _atoi(char *s);
+int _isalpha(int c);
+int _isdelim(char c, char *delimeter);
+
+
+/* Builtin command struct*/
 
 /**
- *struct passinfo - contains pseudo-arguements to pass into a function,
- *		allowing uniform prototype for function pointer struct
- *@arg: a string generated from getline containing arguements
- *@argv: an array of strings generated from arg
- *@path: a string path for the current command
- *@argc: the argument count
- *@line_count: the error count
- *@error_num: the error code for exit()s
- *@linecount_flag: if on count this line of input
- *@fname: the program filename
- *@env: linked list local copy of environ
- *@environ: custom modified copy of environ from LL env
- *@history: the history node
- *@alias: the alias node
- *@env_changed: on if environ was changed
- *@status: the return status of the last exec'd command
- *@cmd_buf: address of pointer to cmd_buf, on if chaining
- *@cmd_buf_type: CMD_type ||, &&, ;
- *@readfd: the fd from which to read line input
- *@histcount: the history line number count
+ * struct _builtin - Defines a struct that conatins built-in commands
+ * with their respective implementation functions
+ * @command: - Built-in command
+ * @function: - Pointer to custom functions that have
+ * similar functionalities as the built-in commands
  */
-
-typedef struct passinfo
+typedef struct _builtin
 {
-	int error_num;
-}info_t
-/*_atoi.c */
-int interactive(info_t *info);
-int _isdelim(char c, char *delimeter);
-int _isalpha(int c);
-int _atoi(char *s);
+	char *command;
+	int (*function)(char **line, int st);
+} builtin;
 
-/* toem_exit.c */
-int _exit(info_t *);
-int _mycd(info_t *);
-int _myhelp(info_t *);
 
 #endif
-
